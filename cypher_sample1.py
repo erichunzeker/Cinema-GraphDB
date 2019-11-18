d67becc5daeceae987fd7bf49eccda9a800729a1
Eric Hunzeker <emh128@pitt.edu>
2019-11-18 16:24:38 -0500

    finished q2

--- cypher_sample1.py
+++ cypher_sample1.py
@@ -11,30 +11,56 @@ transaction = session.begin_transaction()
 
 write_output = open("output.txt", "w")
 
-result = transaction.run("MATCH (people:Person) RETURN people.name LIMIT 10")
-for record in result:
-    print(record['people.name'])
-
 # 1.) List the first 20 actors in descending order of the number of films they acted in.
 # OUTPUT: actor_name, number_of_films_acted_in
 
 write_output.write("### Q1 ###\n")
 
 result = transaction.run("""
-   MATCH (a:Actor)-[:ACTS_IN]->(m:Movie)
-   WITH a, collect(m) AS movies
-   RETURN a.name, length(movies)
-   ORDER BY length(movies) DESC
-   LIMIT 20;
-   """)
+    MATCH (a:Actor)-[:ACTS_IN]->(m:Movie)
+    WITH a, collect(m) AS movies
+    RETURN a.name, length(movies)
+    ORDER BY length(movies) DESC
+    LIMIT 20;
+    """)
 for record in result:
-    line = record['a.name'] + ',' + str(record['length(movies)'])
     write_output.write(record['a.name'] + ',' + str(record['length(movies)']) + '\n')
-    print(record['a.name'], ',', record['length(movies)'])
-transaction.close()
+    # print(record['a.name'], ',', record['length(movies)'])
+
+write_output.write("\n")
 
 # 2.) List the titles of all movies with a review with at most 3 stars.
 # OUTPUT: movie title
 
+write_output.write("### Q2 ###\n")
+
+result = transaction.run("""
+    MATCH (u) -[r:RATED]-> (m:Movie) 
+    WHERE r.stars <= 3 
+    RETURN m.title as mt 
+    """)
+for record in result:
+    write_output.write(record['mt'] + '\n')
+    print(record['mt'])
+
+write_output.write("\n")
+
+# 3.) Find the movie with the largest cast, out of the list of movies that have a review.
+# OUTPUT: movie_title, number_of_cast_members
+
+write_output.write("### Q3 ###\n")
+
+result = transaction.run("""
+    MATCH (u) -[r:RATED]-> (m:Movie) 
+    WHERE r.stars <= 3 
+    RETURN m.title as mt 
+    """)
+for record in result:
+    write_output.write(record['mt'] + '\n')
+    print(record['mt'])
+
+write_output.write("\n")
+
+transaction.close()
 write_output.close()
 session.close()
